c782c3dd9fb86128aee5dcd87b072991feb76267
Nekomamushiii <hurlee.huu132@gmail.com>
2020-08-17 08:43:12 +0800

    hicheel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 require("@babel/polyfill");
 import Search from "./model/search";
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+
+const state = {};
+const controlSearch = async () => {
+  const query = "pizza";
+  if (query) {
+    state.search = new Search(query);
+    await state.search.doSearch();
+    console.log(state.search.result);
+  }
+};
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
